a19de54cf040333114b7eb823170c56e46749a92
lMatteo0311 <levaimate03@gmail.com>
2025-04-29 09:56:23 +0200

    Add project files.

---
+++ Hatvany1/PowerCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatvany1
+{
+    internal class PowerCalculator
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HatvanyTeszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HatvanyTeszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HatvanyTeszt")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f41ce1a0-ef7e-43c3-b2a9-f5a948dfb2e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
